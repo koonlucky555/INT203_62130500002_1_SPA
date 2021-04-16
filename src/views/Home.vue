dbbd6e53a9d98a304bfa7f13e04957e330023820
koonlucky555 <koonlucky123@gmail.com>
2021-04-17 01:32:26 +0700

    [CREATE] All Vue Again

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div id="nav" class="absolute w-full p-12">
+      <router-link to="/" class="text-white hover:text-black text-xl">Home</router-link> |
+      <router-link to="/about" class="text-white hover:text-black text-xl">About</router-link>
+    </div>
+    <div
+      class="w-full h-screen bg-center bg-no-repeat bg-cover"
+      style="
+        background-image: url('https://images.unsplash.com/photo-1544747582-e90fd05971ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80');
+      "
+    >
+      <div
+        class="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center"
+      >
+        <div class="mx-4 text-center text-white">
+          <h1 class="font-bold text-6xl mb-4">Welcome to Lucky KL !</h1>
+          <h2 class="font-bold text-3xl mb-12">
+            Click below to see my website
+          </h2>
+          <div>
+            <a
+              href="/project"
+              class="border-2 border-white rounded-md font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-60 hover:text-white mr-6"
+            >
+              Click here
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
---
+++ src/views/Project.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="w3-black" id="tour">
+  
+
+      <div class="w3-row-padding w3-padding-32" style="margin: 0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img
+            src="/w3images/newyork.jpg"
+            alt="New York"
+            style="width: 100%"
+            class="w3-hover-opacity"
+          />
+          <div class="w3-container w3-white">
+            <p><b>New York</b></p>
+            <p class="w3-opacity">Fri 27 Nov 2016</p>
+            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <button
+              class="w3-button w3-black w3-margin-bottom"
+              onclick="document.getElementById('ticketModal').style.display='block'"
+            >
+              Buy Tickets
+            </button>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img
+            src="/w3images/paris.jpg"
+            alt="Paris"
+            style="width: 100%"
+            class="w3-hover-opacity"
+          />
+          <div class="w3-container w3-white">
+            <p><b>Paris</b></p>
+            <p class="w3-opacity">Sat 28 Nov 2016</p>
+            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <button
+              class="w3-button w3-black w3-margin-bottom"
+              onclick="document.getElementById('ticketModal').style.display='block'"
+            >
+              Buy Tickets
+            </button>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img
+            src="/w3images/sanfran.jpg"
+            alt="San Francisco"
+            style="width: 100%"
+            class="w3-hover-opacity"
+          />
+          <div class="w3-container w3-white">
+            <p><b>San Francisco</b></p>
+            <p class="w3-opacity">Sun 29 Nov 2016</p>
+            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <button
+              class="w3-button w3-black w3-margin-bottom"
+              onclick="document.getElementById('ticketModal').style.display='block'"
+            >
+              Buy Tickets
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+  
+</template>
